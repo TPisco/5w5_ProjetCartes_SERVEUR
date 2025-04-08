d6cedb4487943d6798d15968f5c6076a03a5eb26
Thanh <2355336@cegepmontpetit.ca>
2025-04-07 20:25:59 -0400

    Init : Ajout des propriétés dans Power

--- sci-depart-main/Models/Models/Power.cs
+++ sci-depart-main/Models/Models/Power.cs
@@ -15,5 +15,8 @@ namespace Models.Models
 
         public int Id { get; set; }
 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
     }
 }
