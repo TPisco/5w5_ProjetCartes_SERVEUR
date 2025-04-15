4e6440f051a95af1fa36eef7d383c5e33d40c530
Pisco <6237456@cegepmontpetit.ca>
2025-04-15 10:46:59 -0400

    FCT: Début Création des Évenements

---
+++ sci-depart-main/WebApi/Combat/PowerEvent/ThornsEvent.cs
@@ -0,0 +1,18 @@
+﻿using Super_Cartes_Infinies.Combat;
+using Super_Cartes_Infinies.Models;
+
+namespace WebApi.Combat.PowerEvent
+{
+    public class ThornsEvent : MatchEvent
+    {
+        public override string EventType { get { return "ThornsEvent"; } }
+        public int PlayerId { get; set; }
+
+        public ThornsEvent(MatchPlayerData playerData) 
+        { 
+            PlayerId = playerData.PlayerId;
+
+            // a faire pour HealEvent, ShieldEvent, FirstStrike
+        }
+    }
+}
