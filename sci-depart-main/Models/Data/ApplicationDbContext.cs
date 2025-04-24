345c6d4845310d20f21ce8bce9d81cda498d11b8
thanh <2355336@cegepmontpetit.ca>
2025-04-24 13:07:08 -0400

    Revert "Merge branch 'MVC-AjoutPouvoir' into dev2"
    This reverts commit 643ac1a2ef7fd9ee9b9437f68a78b1ad045a3ac1, reversing
    changes made to 922f9e3e0d528ad558d8360de4d54be019883c21.

--- sci-depart-main/Models/Data/ApplicationDbContext.cs
+++ sci-depart-main/Models/Data/ApplicationDbContext.cs
@@ -32,8 +32,8 @@ public class ApplicationDbContext : IdentityDbContext
         builder.Entity<StartingCards>().HasData(Seed.seedStartingCards());
         builder.Entity<GameConfig>().HasData(Seed.seedGameConfig());
 
-        builder.Entity<CardPower>().HasData(Seed.SeedCardPowers());
         builder.Entity<Power>().HasData(Seed.SeedPower());
+        builder.Entity<CardPower>().HasData(Seed.SeedCardPowers());
 
         // Lorsque le modèle de données se complexifient, il faut éventuellement utiliser Fluent API
         // https://learn.microsoft.com/en-us/ef/ef6/modeling/code-first/fluent/types-and-properties
