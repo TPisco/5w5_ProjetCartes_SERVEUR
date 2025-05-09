498368223cd01afb99e4c47cfde388cfd6b029ba
Rlapre <6223627@cegepmontpetit.ca>
2025-05-08 22:11:22 -0400

    Progress: Ajout du DamageDownEvent (logique à compléter)

--- sci-depart-main/WebApi/Combat/PowerEvent/DamageDownEvent.cs
+++ sci-depart-main/WebApi/Combat/PowerEvent/DamageDownEvent.cs
@@ -1,9 +1,43 @@
-﻿using Super_Cartes_Infinies.Combat;
+﻿using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Models.Models;
+using Super_Cartes_Infinies.Combat;
+using Super_Cartes_Infinies.Models;
 
 namespace WebApi.Combat.PowerEvent
 {
     public class DamageDownEvent : MatchEvent
     {
         public override string EventType => "DamageDown";
+
+
+        public int TargetCardId { get; set; }
+        public int PlayerId { get; set; }
+
+        public int Value { get; set; }
+
+
+        public DamageDownEvent(PlayableCard defendingCard, MatchPlayerData defender)
+        {
+            PlayerId = defender.PlayerId;
+            TargetCardId = defendingCard.Id;
+            Value = defendingCard.GetStatusValue(Status.POISONX_ID);
+           // var reducedDmgValue = 
+            var oldDmg = defendingCard.Attack;
+            //Valeur de l'effet de poison
+            CardStatus status = defendingCard.CardStatus.Where(c => c.StatusId == Status.POISONX_ID).First();
+            //TODO : Vérifier la logique du CardDamageEvent
+            defendingCard.Attack = oldDmg - Value;
+            //Réduire la valeur du poison. Si le poison est = à 0, retirer le status de la carte
+            if (status.Value - 1 <= 0)
+            {
+                defendingCard.CardStatus.Remove(status);
+            }
+            else
+            {
+                status.Value--;
+            }
+        }
+
     }
+}
 }
